79d4cb866f617236268220ab7ab38b946b200806
X10 <x10@X10-CFY3D027HK.local>
2024-06-04 15:56:35 +0700

    my

--- cypress.config.js
+++ cypress.config.js
@@ -18,13 +18,8 @@ module.exports = defineConfig({
 
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      return require('./cypress/plugins/index.js')(on,config)
     },
+    baseUrl: "https://react-redux.realworld.io/"
   },
-  video: true
-
-
-
-
-
 });
